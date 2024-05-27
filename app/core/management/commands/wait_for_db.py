826d8932921685c4c5850101fdcb820c47f7d809
mubashar.rashid@vaival.tech <mubashar.rashid@vaival.tech>
2024-05-27 12:33:07 +0500

    video 39

--- app/core/management/commands/wait_for_db.py
+++ app/core/management/commands/wait_for_db.py
@@ -1,5 +1,8 @@
-from django.core.management.base import BaseCommand
+import time
+from psycopg2 import OperationalError as Psycopg2Error
+from django.db.utils import OperationalError
 
+from django.core.management.base import BaseCommand
 
 class Command(BaseCommand):
     def handle(self, *args, **options):
