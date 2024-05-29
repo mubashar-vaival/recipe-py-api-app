cca964da518135ac889cfe29f20db6ad9edb0c0e
mubashar.rashid@vaival.tech <mubashar.rashid@vaival.tech>
2024-05-29 15:49:32 +0500

    user API

--- app/user/tests/test_user_api.py
+++ app/user/tests/test_user_api.py
@@ -10,9 +10,6 @@ from rest_framework import status
 
 
 CREATE_USER_URL = reverse('user:create')
-TOKEN_URL = reverse('user:token')
-ME_URL = reverse('user:me')
-
 
 def create_user(**params):
     """Create and return a new user."""
@@ -65,21 +62,3 @@ class PublicUserApiTests(TestCase):
             email=payload['email']
         ).exists()
         self.assertFalse(user_exists)
-
-    def test_create_token_for_user(self):
-        """Test generates token for valid credentials."""
-        user_details = {
-            'name': 'Test Name',
-            'email': 'test@example',
-            'password': 'test-user-password123',
-        }
-        create_user(**user_details)
-
-        payload = {
-            'email': user_details['email'],
-            'password': user_details['password'],
-        }
-        res = self.client.post(TOKEN_URL, payload)
-
-        self.assertIn('token', res.data)
-        self.assertEqual(res.status_code, status.HTTP_200_OK)
